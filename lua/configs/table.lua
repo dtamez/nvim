8a12d8c73422521581cc55348528fe6f40bd88a2
dtamez <zematynnad@gmail.com>
2026-06-18 17:58:21 -0500

    Change table mappings so as not to conflict with tmux.

---
+++ lua/configs/table.lua
@@ -0,0 +1,20 @@
+return {
+    mappings = {
+        next = "<leader>tn",
+        prev = "<leader>tp",
+        insert_table = "<leader>tt",
+        insert_table_alt = "<leader>tT",
+
+        insert_row_up = "<leader>tk",
+        insert_row_down = "<leader>tj",
+        move_row_up = "<leader>tK",
+        move_row_down = "<leader>tJ",
+
+        insert_column_left = "<leader>th",
+        insert_column_right = "<leader>tl",
+        move_column_left = "<leader>tH",
+        move_column_right = "<leader>tL",
+
+        delete_column = "<leader>td",
+    },
+}
